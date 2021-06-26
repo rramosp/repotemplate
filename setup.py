134fcce5b60ad6498ee8e7b5c02e85d02a74fe57
rramosp <rramosp@unal.edu.co>
2021-06-25 19:05:20 -0500

    iteration

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages, find_namespace_packages
 setup(
     name = 'repotemplate',
     version = '0.1.0',
-    package_dir = {"": "src"}
+    package_dir = {"": "src"},
+    packages = ['repotemplate']
 )
